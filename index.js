38fedff6f3c411497bd3e2e0f6d12c563f426ed3
destiaarti <54435481+destiaarti@users.noreply.github.com>
2019-10-06 17:40:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import List from './List';
-import Tugas from './Tugas';
+import Tugas from './Make_list';
 // flatlist
 //pullrefresh
-AppRegistry.registerComponent(appName, () => Tugas);
+AppRegistry.registerComponent(appName, () => Make_list);
